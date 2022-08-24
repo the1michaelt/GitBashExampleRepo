0f391b194f9130a8b86895da3995b8a169f99b93
Michael Thompson <thompsonmichaelj2020@gmail.com>
2022-08-24 13:00:50 -0400

    alarm_clock.py

---
+++ Desktop/devCodeCamp/alarm_clock/alarm_clock.py
@@ -0,0 +1,39 @@
+class AlarmClock:
+    def __init__(self):
+        # current time = ""
+        # whether the alarm is on or off =['on','off']
+        # time the alarm is set to = ""
+        
+    def current_time(self):
+        pass
+    
+    def alarm_on_off(self):
+        pass
+    
+    def time_of_alarm(self):
+        pass
+    
+    def set_or_change_current_time(self):
+        pass 
+#
+
+# class to have class variables to keep track of the AlarmClock’s 
+# current time, 
+# whether the alarm is on or off,  
+# time the alarm is set to.
+# NOTE: You can use arbitrary strings to represent the time, it does not need to be accurate or change over time
+
+# method to set (or change) the current time and 
+# print to the console the current time. return ___
+
+# toggle the alarm on or off. 
+
+# a method to set the current alarm time  input?
+# and print to the console the current alarm time.  print or return ()
+
+# import the AlarmClock class into main.py so I can instantiate it as a new AlarmClock object and call methods on it.
+
+# Print the clock’s time to the terminal print(___)
+
+# Call the alarm clock’s change time method to change the time
+
