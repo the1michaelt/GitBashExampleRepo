510802e1e6a9735740ddfd1caaa13e554c877c07
Michael Thompson <thompsonmichaelj2020@gmail.com>
2022-09-18 08:42:19 -0400

    for instructor review: mvp results; more methods needed

--- pair_players_new202209.py
+++ pair_players_new202209.py
@@ -21,30 +21,32 @@ shuffled_deck= randomize_cards(create_deck, random)
 # 3. Deals five cards to four players (concurrent)
 def deal_to_player(shuffled_deck):
     player = 1
-    while player <= 5:
+    while player <= 4:
         cards = 0
         deal_hand = []
         while cards < 5:
             deal_card = shuffled_deck.pop(0)
             deal_hand.append(deal_card)
             cards += 1
-            print(f'Player {player}:{deal_hand}\n')
+            # print(f'Player {player}:{deal_hand}\n')
             # return deal_hand
         # check.determine_if_pairs()
             # deal_hand = deal_five_to_each
             if len(deal_hand)== 5:
                 check_card = 1
+                pair_count=0
+                print(f'\nPlayer {player}:{deal_hand}', end=" ")
                 while check_card <= 3:
-                    for this_card in deal_hand:
+                    for this_card in deal_hand:                        
                         pairs = deal_hand.count(this_card)
                         if pairs > 1:
-                            print(f'Player {player}: the {this_card} is paired.\n')                                                      
+                            print(f'The {this_card} is paired.', end = " ")
+                            pair_count += 0.5
                     check_card += 1
-                    print(f'Player {player}: there are no more pairs.\n')
+                    print(f'\nPlayer {player} has {pair_count} pair(s).')
                     break
         player += 1
 deal_five_to_each= deal_to_player(shuffled_deck)
-
 
         #    if len(deal_hand) == 5:
         #        check_card = 1
@@ -59,64 +61,64 @@ deal_five_to_each= deal_to_player(shuffled_deck)
 
 
 
-# 4. checking for pairs
-def determine_if_pairs():
-    # hand = deal_five_to_each
-    first_card = deal_hand[0]  # Get the first card
-    # Compares all the elements with the first card
-    for card in hand:
-        if first_card != card:  # not true
-            print("There are no pairs.")
-            break
-        else:
-            print("There are pairs.")
-
-
-hand_pairs = determine_if_pairs()
-
-
-
-# # 3. Deals five cards to four players (concurrent) 
-# def deal_to_player(shuffled_deck):
-#     player = 1
-#     while player <= 5:
-#         cards = 0
-#         deal_hand = []
-#         while cards < 5:
-#             deal_card = shuffled_deck.pop(0)
-#             deal_hand.append(deal_card)
-#             cards += 1
-#             # return deal_hand
-#         print(f'Player {player}:{deal_hand}\n')
-#         check.determine_if_pairs()
-#         player += 1
-    
-# # deal_five_to_each= deal_to_player(shuffled_deck)
-
-# # 4. checking for pairs 
+# # 4. checking for pairs
 # def determine_if_pairs():
-#     hand = deal_five_to_each
-#     first_card = hand[0]  # Get the first card
+#     # hand = deal_five_to_each
+#     first_card = deal_hand[0]  # Get the first card
 #     # Compares all the elements with the first card
 #     for card in hand:
-#         if first_card != card: #not true
+#         if first_card != card:  # not true
 #             print("There are no pairs.")
 #             break
 #         else:
 #             print("There are pairs.")
 
+
 # hand_pairs = determine_if_pairs()
 
 
-# 3. select hand of 5 cards
-# def deal_five_cards(shuffled_deck):
-#     i = 0
-#     deal_hand = []
-#     while i < 5:
-#         deal_card = shuffled_deck.pop(0)
-#         deal_hand.append(deal_card)
-#         i += 1
-#         return deal_hand
 
-# hand_for_each = deal_five_cards(shuffled_deck)
+# # # 3. Deals five cards to four players (concurrent) 
+# # def deal_to_player(shuffled_deck):
+# #     player = 1
+# #     while player <= 5:
+# #         cards = 0
+# #         deal_hand = []
+# #         while cards < 5:
+# #             deal_card = shuffled_deck.pop(0)
+# #             deal_hand.append(deal_card)
+# #             cards += 1
+# #             # return deal_hand
+# #         print(f'Player {player}:{deal_hand}\n')
+# #         check.determine_if_pairs()
+# #         player += 1
+    
+# # # deal_five_to_each= deal_to_player(shuffled_deck)
+
+# # # 4. checking for pairs 
+# # def determine_if_pairs():
+# #     hand = deal_five_to_each
+# #     first_card = hand[0]  # Get the first card
+# #     # Compares all the elements with the first card
+# #     for card in hand:
+# #         if first_card != card: #not true
+# #             print("There are no pairs.")
+# #             break
+# #         else:
+# #             print("There are pairs.")
+
+# # hand_pairs = determine_if_pairs()
+
+
+# # 3. select hand of 5 cards
+# # def deal_five_cards(shuffled_deck):
+# #     i = 0
+# #     deal_hand = []
+# #     while i < 5:
+# #         deal_card = shuffled_deck.pop(0)
+# #         deal_hand.append(deal_card)
+# #         i += 1
+# #         return deal_hand
+
+# # hand_for_each = deal_five_cards(shuffled_deck)
 
