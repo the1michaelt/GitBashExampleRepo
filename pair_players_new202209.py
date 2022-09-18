55e65ea493e7d71871c28de656ab5c2fd678efbd
Michael Thompson <thompsonmichaelj2020@gmail.com>
2022-09-17 22:42:54 -0400

    feat: deals five cards to four players

--- pair_players_new202209.py
+++ pair_players_new202209.py
@@ -17,35 +17,31 @@ def randomize_cards(create_deck, random):
 
 shuffled_deck= randomize_cards(create_deck, random)
 
-#3. select hand of 5 cards
-def deal_hand_to_players(shuffled_deck):
-    i = 0
-    deal_hand = []
-    while i < 5:
-        deal_card = shuffled_deck.pop()
-        deal_hand.append(deal_card)
-        i += 1
-        return deal_hand
- 
-hand_for_each = deal_hand_to_players(shuffled_deck)
+# 3. Deals five cards to four players (concurrent) 
+def deal_to_player(shuffled_deck):
+    player = 1
+    while player <= 5:
+        cards = 0
+        deal_hand = []
+        while cards < 5:
+            deal_card = shuffled_deck.pop(0)
+            deal_hand.append(deal_card)
+            cards += 1
+            # return deal_hand
+        print(f'Player {player}:{deal_hand}\n')
+        player += 1
+        
+deal_five_to_each= deal_to_player(shuffled_deck)
 
-#4. 
-
-
-# #3. deal hand of 5 cards to four players
-# def deal_hand_to_players(shuffled_deck, random):
+# 3. select hand of 5 cards
+# def deal_five_cards(shuffled_deck):
 #     i = 0
-#     while i < 3:  # deals 4 times
-#         while i < 4:
-#             deal_deck = []
-#             deal_deck += shuffled_deck
-#             i += 1
-#             print(deal_deck)
+#     deal_hand = []
+#     while i < 5:
+#         deal_card = shuffled_deck.pop(0)
+#         deal_hand.append(deal_card)
 #         i += 1
+#         return deal_hand
 
+# hand_for_each = deal_five_cards(shuffled_deck)
 
-# deal_hand = deal_hand_to_players(shuffled_deck, random)
-
-
-
-#4. 
